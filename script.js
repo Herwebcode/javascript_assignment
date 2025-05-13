44fa3f8108d899121f52c37b2777875bd9a9a0db
Oyekunle Bashirat <122808225+Herwebcode@users.noreply.github.com>
2025-05-13 15:56:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
 function changeText() {
-  document.getElementById("info-text").textContent =
-    "The text has been changed dynamically!";
+  document.getElementById("main-heading").innerText = "You clicked the button!";
 }
 
-function toggleStyle() {
-  const title = document.getElementById("main-title");
-  title.classList.toggle("highlight");
-}
-
-function addElement() {
-  const newPara = document.createElement("p");
-  newPara.textContent = "This paragraph was added dynamically!";
-  newPara.id = "new-paragraph";
-  document.getElementById("dynamic-content").appendChild(newPara);
-}
-
-function removeElement() {
-  const existingPara = document.getElementById("new-paragraph");
-  if (existingPara) {
-    existingPara.remove();
-  } else {
-    alert("No element to remove!");
-  }
-}
-function changeText() {
-  document.getElementById("info-text").textContent = "The text has been changed dynamically!";
-}
-
-function toggleStyle() {
-  const title = document.getElementById("main-title");
-  title.classList.toggle("highlight");
+function changeColor() {
+  const box = document.getElementById("box");
+  box.style.backgroundColor = "lightgreen";
+  box.style.color = "darkblue";
 }
 
 function addElement() {
-  const newPara = document.createElement("p");
-  newPara.textContent = "This paragraph was added dynamically!";
-  newPara.id = "new-paragraph";
-  document.getElementById("dynamic-content").appendChild(newPara);
+  const newElement = document.createElement("p");
+  newElement.id = "new-paragraph";
+  newElement.innerText = "A new paragraph was added!";
+  document.body.appendChild(newElement);
 }
 
 function removeElement() {
-  const existingPara = document.getElementById("new-paragraph");
-  if (existingPara) {
-    existingPara.remove();
-  } else {
-    alert("No element to remove!");
+  const element = document.getElementById("new-paragraph");
+  if (element) {
+    element.remove();
   }
 }
